f1b987ee57585835ab6895ea8b7dcfad9d8dab07
Ayushgh1017 <75933380+Ayushgh1017@users.noreply.github.com>
2023-06-26 10:05:01 +0530

    Update task7.sql

--- task7.sql
+++ task7.sql
@@ -1,1 +1,14 @@
-
+-- QUERY 7
+CREATE OR REPLACE FUNCTION no_participants(in t_year int,OUT nums int)
+LANGUAGE plpgsql
+AS
+$$ 
+BEGIN
+  SELECT
+      COUNT(*) INTO nums
+  FROM tournament_entry
+  WHERE tournament_entry.year=t_year;
+END;
+$$;
+select * from no_participants(2022);
+select * from tournament
