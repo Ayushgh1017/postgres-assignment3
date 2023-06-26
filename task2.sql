5357297ef123215dcac0b712d45dbe048644bb26
Ayushgh1017 <75933380+Ayushgh1017@users.noreply.github.com>
2023-06-26 10:03:06 +0530

    Update task2.sql

--- task2.sql
+++ task2.sql
@@ -1,1 +1,17 @@
+-- QUERY 2
 
+CREATE OR REPLACE FUNCTION tournament(IN tour_year int, OUT no_of_t int) 
+--RETURNS int  
+LANGUAGE plpgsql  
+AS  
+$$  
+DECLARE  
+    --number_of_tournaments integer;  
+BEGIN  
+    select COUNT(*) into no_of_t from tournament_entry  where 
+	tour_year = year GROUP BY year;
+    --RETURN number_of_tournaments;  
+End;  
+$$; 
+SELECT * from tournament(2023);
+select * from tournament_entry;
