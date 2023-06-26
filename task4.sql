b6f75bcb368842d599c0692b6b3ce95424186982
Ayushgh1017 <75933380+Ayushgh1017@users.noreply.github.com>
2023-06-26 10:04:02 +0530

    Update task4.sql

--- task4.sql
+++ task4.sql
@@ -1,1 +1,14 @@
+-- QUERY 4
 
+create or replace procedure manager_details1(manager_name varchar(100),team_name varchar(100))
+language plpgsql
+as $$
+-- declare firstname int;
+-- declare teamname int;
+begin
+    update member set firstname = manager_name where teamid = 
+	(select teamid from team where teamname = team_name and memberid = manager);
+end; $$
+CALL manager_details1('Ameya', 'Team A');
+CALL manager_details1('Aman', 'Team B');
+select * from member;
