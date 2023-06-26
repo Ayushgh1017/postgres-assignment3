87cb4bc93835d2dacd64ba5d0164cc808b5338ad
Ayushgh1017 <75933380+Ayushgh1017@users.noreply.github.com>
2023-06-26 10:02:45 +0530

    Update task1.sql

--- task1.sql
+++ task1.sql
@@ -1,1 +1,19 @@
+-- QUERY 1
 
+CREATE OR REPLACE PROCEDURE entry(member_name varchar, tournament_name varchar, tyear INT)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    memid INT;
+    tid INT;
+BEGIN
+    SELECT memberid INTO memid FROM member where firstname=member_name;
+    SELECT tourid INTO tid FROM tournament where name = tournament_name;
+    
+    INSERT INTO tournament_entry (memberid, tourid, year)
+    VALUES (memid, tid, tyear);
+END;
+$$;
+call entry('Sachin','Tournament 2',2022)
+select * from member;
+select * from tournament_entry
